54c8b9b0050c0c31540a7f5c3af2b1758b0316a9
shery15 <382254994@qq.com>
2018-07-24 22:41:12 +0800

    feat: add handle to server

--- demo1/index.js
+++ demo1/index.js
@@ -1,5 +1,10 @@
 const server = require('./server');
 const router = require('./router')
+const requestHandlers = require('./requestHandlers')
 
+const handle = {}
+handle['/'] = requestHandlers.start
+handle['/start'] = requestHandlers.start
+handle['/upload'] = requestHandlers.upload
 
-server.startHTTPServer(router.route);
+server.startHTTPServer(router.route, handle);
